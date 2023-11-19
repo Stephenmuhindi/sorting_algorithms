7bdb161c339647c6dd8e15f9c8387f0d4c1e06b3
Stephenmuhindi <bteneson42@gmail.com>
2023-11-19 12:15:04 -0800

    we

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void selection_sort(int *array, size_t size);
 size_t partition(int **array, size_t low, size_t high, size_t size);
 void qS(int **array, size_t low, size_t high, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
